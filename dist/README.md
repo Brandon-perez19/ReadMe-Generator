a55730fb9a144a4c70235c6c7eed6d9f5f9f0b1b
Brandon <brandonperez@brandons-mbp.lan>
2022-06-21 22:35:42 -0400

    fixed error in readme structure that caused indentation of license section.

--- dist/README.md
+++ dist/README.md
@@ -1,8 +1,8 @@
 
-  # r
+  # t
 
   ## Description <br>
-  r
+  t
 
   ## Table Of Contents <br>
   [Installation](#installation) <br>
@@ -11,22 +11,25 @@
   [License](#license)
 
   ## Installation <br>
-  r
+  t
 
   ## Usage <br>
-  r
+  t
 
   ## Questions <br>
-  r <br>
-  github.com//r <br>
-  Any questions please contact: r <br> 
-    ## License <br>
-    GNU AGPLv3 <br>
-    ## Badges <br>
-    [![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0) (https://www.gnu.org/licenses/agpl-3.0) <br>
-    
+  t <br>
+  github.com//t <br>
+  Any questions please contact t <br>
+
   ## How to Contribute <br>
-  r
+  t
 
   ## Tests <br>
-  r 
+  t
+  
+  ## License <br>
+  GNU AGPLv3 <br>
+  
+  ## Badges <br>
+  [![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)] (https://www.gnu.org/licenses/agpl-3.0) <br>
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,42 +4,42 @@ const licenseObj = [
   {
     license: "GNU AGPLv3",
     url: "(https://www.gnu.org/licenses/agpl-3.0)",
-    badge: '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)'
+    badge: '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)]'
   },
   {
     license: "GNU GPLv3",
     url: "(https://www.gnu.org/licenses/gpl-3.0)",
-    badge: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+    badge: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]'
   },
   {
     license: "GNU LGPLv3",
     url: "(https://www.gnu.org/licenses/lgpl-3.0)",
-    badge: '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)'
+    badge: '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)]'
   },
   {
     license: "Mozilla Public License 2.0",
     url: "(https://opensource.org/licenses/MPL-2.0)",
-    badge: '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+    badge: '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]'
   },
   {
     license: "Apache License 2.0",
     url: "(https://opensource.org/licenses/Apache-2.0)",
-    badge: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+    badge: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]'
   },
   {
     license: "MIT License",
     url: "(https://opensource.org/licenses/MIT)",
-    badge: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+    badge: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]'
   },
   {
     license: "Boost Software License 1.0",
     url: "(https://www.boost.org/LICENSE_1_0.txt)",
-    badge: '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
+    badge: '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]'
   },
   {
     license: "The Unlicense",
     url: "(http://unlicense.org/)",
-    badge: '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)'
+    badge: '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]'
   },
 ]
 
@@ -88,24 +88,24 @@ function renderLicenseSection(license) {
   else {
     var licenseBadge = renderLicenseBadge(license)
     var licenseLink = renderLicenseLink(license);
-    var licenseSection = `
-    ## License <br>
-    ${license} <br>` 
+    var licenseSection = 
+  `
+  ## License <br>
+  ${license} <br>
+  ` 
 
-    var licenseSection2 = `
-    ## Badges <br>
-    ${licenseBadge} ${licenseLink} <br>
-    `
-    // console.log(licenseSection)
+    var licenseSection2 = 
+  `
+  ## Badges <br>
+  ${licenseBadge} ${licenseLink} <br>
+  `
+    
     return licenseSection + licenseSection2
   }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-
-  var licenseSection = renderLicenseSection(data.license)
-
   return `
   # ${data.title}
 
@@ -127,20 +127,16 @@ function generateMarkdown(data) {
   ## Questions <br>
   ${data.username} <br>
   github.com//${data.username} <br>
-  Any questions please contact: ${data.email} <br> `
+  Any questions please contact ${data.email} <br>
 
-  +
-
-  licenseSection
-
-  +
-
-  `
   ## How to Contribute <br>
   ${data.contribution}
 
   ## Tests <br>
-  ${data.test} `;
+  ${data.test}
+  ` +  
+  
+  renderLicenseSection(data.license) ;
 }
 
 export {generateMarkdown}
